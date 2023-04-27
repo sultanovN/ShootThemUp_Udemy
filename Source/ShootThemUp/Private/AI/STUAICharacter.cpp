72d03d92c8698640cbd72da6719ae4e86e11ed47
sultanovN <104188736+sultanovN@users.noreply.github.com>
2023-04-27 14:14:46 +0600

    moving ai character

---
+++ Source/ShootThemUp/Private/AI/STUAICharacter.cpp
@@ -0,0 +1,11 @@
+// Shoot Them Up Game. All rights reserved.
+
+
+#include "AI/STUAICharacter.h"
+#include "AI/STUAIController.h"
+
+ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit) : Super(ObjInit) 
+{
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+	AIControllerClass = ASTUAIController::StaticClass();
+}
---
+++ Source/ShootThemUp/Public/AI/Tasks/STUNextLocationTask.h
@@ -0,0 +1,17 @@
+// Shoot Them Up Game. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "STUNextLocationTask.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API USTUNextLocationTask : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
